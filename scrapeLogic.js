571357cea0337a9ffce2c7b7f4f32babb9254eb0
SaveMeASpark20 <legchris12@gmail.com>
2025-03-13 20:42:07 +0800

    change the scrapeLogic

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,14 +17,14 @@ const scrapeLogic = async (res) => {
     const page = await browser.newPage();
     await page.goto("https://developer.chrome.com/");
     await page.setViewport({ width: 1080, height: 1024 });
-    await page.type(".search-box__input", "automate beyond recorder");
+    await page.type(".devsite-search-field", "automate beyond recorder");
 
-    const searchResultSelector = ".search-box__link";
+    const searchResultSelector = ".devsite-result-item-link";
     await page.waitForSelector(searchResultSelector);
     await page.click(searchResultSelector);
 
     const textSelector = await page.waitForSelector("text/Customize and automate");
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    const fullTitle = await textSelector?.evaluate((el) => el.textContent);
 
     const logStatement = `The title of this blog post is ${fullTitle}`;
     console.log(logStatement);
